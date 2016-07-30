cc55045fa1feceb9f3bb19efa479fe2f2441d867
lanou <lan@hexiaofeidemac-mini.local>
2016-07-30 19:35:54 +0800

    XF第一次初始化

---
+++ Share2/Share2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share2
+//
+//  Created by lanou on 16/7/30.
+//  Copyright © 2016年 he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Share2/Share2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Share2
+//
+//  Created by lanou on 16/7/30.
+//  Copyright © 2016年 he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
