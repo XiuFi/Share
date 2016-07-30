0035b2a2e706f4a3cadbf1121b63e55a11181415
lanou <lan@gzzhao.local>
2016-07-30 19:45:46 +0800

    fuck you

---
+++ Share2/Share2/Fuck.h
@@ -0,0 +1,13 @@
+//
+//  Fuck.h
+//  Share2
+//
+//  Created by lanou on 16/7/30.
+//  Copyright © 2016年 he. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fuck : NSObject
+
+@end
---
+++ Share2/Share2/Fuck.m
@@ -0,0 +1,13 @@
+//
+//  Fuck.m
+//  Share2
+//
+//  Created by lanou on 16/7/30.
+//  Copyright © 2016年 he. All rights reserved.
+//
+
+#import "Fuck.h"
+
+@implementation Fuck
+
+@end
